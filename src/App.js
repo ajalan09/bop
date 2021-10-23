418f0c612a5063cdf942128b00066d1daf08713a
ajalan <ajalan@umich.edu>
2021-10-23 14:56:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          'Srivastav sucks at fortnite'
         </p>
         <a
           className="App-link"
