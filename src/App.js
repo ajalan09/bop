cee58b15ddaf98f897e33a8df93fb397dc100cc7
Bhargavi Ravishankar <bhargavi@Bhargavis-MacBook-Pro.local>
2021-10-24 21:09:46 -0400

    before POST request to backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [picture,setPicture] = useState('');
+  let fileSelectedHandler = (event) => {
+    setPicture(event.target.files[0]);
+    // picture = event.target.files[0]
+  }
+
+  let sendPictureToBackend = (e) => {
+    /*if(picture === ''){
+    }*/
+    e.preventDefault();
+    /*const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(picture)
+    };
+    fetch('https://localhost3000/bop', requestOptions)
+        .then(response => response.json())
+        .then(data => setPostId(data.id));*/
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          'Srivastav sucks at fortnite'
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="file" onChange={fileSelectedHandler}/>
+        <button onClick={sendPictureToBackend}> Upload Picture </button>
       </header>
     </div>
   );
